0edee3338bea4c190b4ba8bc8092c631ca5ced51
yangjung-woo <yee777222000@gmail.com>
2022-03-12 16:11:08 +0900

    3.12 add commit resolv error

--- boj week2/1022_tonado.py
+++ boj week2/1022_tonado.py
@@ -1,7 +1,7 @@
 r1,c1,r2,c2 =list(map(int, input().split())) # 좌측 최상단1 우측 최하단2
 dx =[1,0,-1,0]
 dy =[0,1,0,-1]
-board= [[0]*(c2-c1+1)for _ in range(r2-r1+1) ] #초기화
+board= [[0]* (c2-c1+1)for _ in range(r2-r1+1) ] #초기화
 number_of_board = (c2-c1+1)*(r2-r1+1) #board 수 
 y=x=0
 num=1
@@ -10,7 +10,7 @@ dcnt=1 # 한 방향에서 움직여야 하는 횟수
 d=0
 
 while number_of_board >0:
-    if r1 <=y<= r2 and c1<=0<=c2:
+    if r1 <=y<= r2 and c1<=x<=c2:
         number_of_board-=1 
         board[y-r1][x-c1]=num
         max_num =num
@@ -27,4 +27,4 @@ max_num_len= len(str(max_num-1)) ##쵀개 길이만큼만 출력
 for i in range(r2-r1+1):
     for j in range(c2-c1+1):
         print(str(board[i][j]).rjust(max_num_len),end=' ')
-print()
+    print()
