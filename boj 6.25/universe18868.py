0a434ca3ab93c57c69a50d3fa50f5249a8b9bced
양정우 <yee777222000@gmail.com>
2022-06-25 17:29:10 +0900

    6/25 study

---
+++ boj 6.25/universe18868.py
@@ -0,0 +1,14 @@
+m, n = map(int, input().split())
+universe = [list(map(int, input().split())) for _ in range(m)]
+cnt =0
+for i in range(m):
+  arr_sort = sorted(universe[i])
+  idx=[]
+  for j in universe[i]:
+    idx.appen(arr_sort.index(j)+1)
+  universe[i]=idx
+      
+for i in range(m-1):
+    for j in range(i+1,m):
+        if (universe[i]==universe[j]): cnt+=1
+print(cnt)
