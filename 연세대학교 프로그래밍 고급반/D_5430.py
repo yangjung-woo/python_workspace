b5d83fe20d9554f3c5b089f3b3d4b95661ba42c2
양정우 <yee777222000@gmail.com>
2022-07-07 21:49:33 +0900

    7.7

---
+++ 연세대학교 프로그래밍 고급반/D_5430.py
@@ -0,0 +1,50 @@
+from collections import deque
+from ntpath import join
+import queue
+import sys
+
+def main():
+    T = int(sys.stdin.readline())
+
+    for _ in range(T):
+        commands = sys.stdin.readline().rstrip()
+        N =  int(sys.stdin.readline())
+        arr = sys.stdin.readline().rstrip()[1:-1].split(',')  # [1:-1] 1 부터 N-1까지("[]"생략)
+        queue = deque(arr)
+        if N==0:
+            queue = []
+        
+        
+        flag=do_work(commands, N, queue)
+
+        if (flag==False):
+            print("error")
+        else:
+            print('['+','.join(queue)+']')
+
+def do_work(commands, lenth , arr): 
+    one_more_jump =False
+    for k in range(len(commands)):
+
+        if one_more_jump==True:
+            one_more_jump=False
+            continue
+        else:
+            if commands[k] =='R' and commands[k+1]=='R': # R 2번 연속시 동작 x
+             one_more_jump =True
+             continue
+
+            else:
+
+                if commands[k] == 'R': #뒤집기
+                    arr.reverse()
+
+                elif commands[k] == 'D': # 버리기 
+                    if len(arr) >0:
+                        arr.popleft()
+                    else:
+                        return False
+    return True
+
+if __name__ == '__main__':
+    main()
