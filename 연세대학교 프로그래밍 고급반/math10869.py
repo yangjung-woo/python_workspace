e8fc5d94b72dcfd0d1e86077f26458f8aac0f830
양정우 <yee777222000@gmail.com>
2022-07-16 14:54:59 +0900

    7.16

---
+++ 연세대학교 프로그래밍 고급반/math10869.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def main():
+    a,b = map(int,sys.stdin.readline().split())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(a//b)
+    print(a%b)
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
