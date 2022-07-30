ea22364f17e6125c124e5815793a5555f4b9cc3c
양정우 <yee777222000@gmail.com>
2022-07-30 22:03:24 +0900

    7.30

--- boj 완전탐색 bronze/Four-Squares_17626.py
+++ boj 완전탐색 bronze/Four-Squares_17626.py
@@ -1,7 +1,20 @@
+
 def main():
     N = int(input())
-    if N %2 ==1: print("SK")
-    else: print("CY")   
+    dp =[0 for i in range(N+1)]
+    dp[1]=1
+   
+    for i in range(2,N+1):
+        min_value = 1e9 
+        j=1
+        while (j**2) <= i:
+            min_value = min(min_value, dp[i - (j**2)])
+            j += 1
+        dp[i]= min_value+1
+    print(dp[N])
+
+
+
 
 
 if __name__ == '__main__':
